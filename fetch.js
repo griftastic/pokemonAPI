1c44bfb042586024e29e2fb56bfa5a557bb825fe
Griffin Brown <browgrif@gmail.com>
2022-02-15 22:01:42 -0500

    Drag and Drop

--- fetch.js
+++ fetch.js
@@ -1,9 +1,19 @@
+//Declarations
+
 const baseURL = "https://pokeapi.co";
 
+const fill = document.querySelector('.cardOne')  
+const fill2 = document.querySelector('.cardTwo') 
+const fill3 = document.querySelector('.cardThree') 
+const empties = document.querySelectorAll('.empty')
+
+//RNG
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
+
+//API Fetch / Appending Cards
 
 function fetchPokemon() {
     for(let x = 1; x<=3; x++){
@@ -22,3 +32,48 @@ function fetchPokemon() {
 }
 
 fetchPokemon()
+
+//Drag and Drop
+
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+    // let newDropZone = document.createElement("section.dropZone");
+    // document.append(newDropZone);
+}
+
+function dragEnd() {
+    this.className = 'contentCard';
+}
+
+for (const empty of empties){
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+// fill2.addEventListener('dragstart', dragStart);
+// fill2.addEventListener('dragend', dragEnd);
+// fill3.addEventListener('dragstart', dragStart);
+// fill3.addEventListener('dragend', dragEnd);
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered'
+}
+
+function dragLeave() {
+    // this.className = 'empty'
+}
+
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill);
+}
